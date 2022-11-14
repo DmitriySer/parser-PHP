ef991e6a274a69c6b046296d2599f6e185547f50
DmitriySer <111710614+DmitriySer@users.noreply.github.com>
2022-11-14 17:03:35 +0400

    up

--- parser.sql
+++ parser.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 14 2022 г., 15:47
+-- Время создания: Ноя 14 2022 г., 16:03
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.2.34
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `parserTab` (
+  `id` int NOT NULL,
   `number` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `date` datetime NOT NULL,
   `link` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -35,6 +36,26 @@ CREATE TABLE `parserTab` (
   `status` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `price` decimal(10,0) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `parserTab`
+--
+ALTER TABLE `parserTab`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `parserTab`
+--
+ALTER TABLE `parserTab`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
